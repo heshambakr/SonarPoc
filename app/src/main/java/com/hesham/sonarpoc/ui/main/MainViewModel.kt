47ba3786acefeec50c73125d18289b6fe804ec70
Hesham Bakr <hesham.bakr@talabat.com>
2020-09-10 16:54:57 +0400

    initial commit

--- app/src/main/java/com/hesham/sonarpoc/ui/main/MainViewModel.kt
+++ app/src/main/java/com/hesham/sonarpoc/ui/main/MainViewModel.kt
@@ -5,6 +5,6 @@ import androidx.lifecycle.ViewModel
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
     fun notCoveredByTest(){
-        print("hello2")
+        print("hello22")
     }
 }
