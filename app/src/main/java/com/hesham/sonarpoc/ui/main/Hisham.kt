19fe3279de505becf1c286e3a78e7d63e842a5fc
Hesham Bakr <hesham.bakr@talabat.com>
2020-09-10 17:03:55 +0400

    progress

---
+++ app/src/main/java/com/hesham/sonarpoc/ui/main/Hisham.kt
@@ -0,0 +1,6 @@
+package com.hesham.sonarpoc.ui.main
+
+class Hisham {
+    fun add(a:Int, b: Int) = a+b
+    fun add2(a:Int, b: Int) = a+b
+}
---
+++ app/src/test/java/com/hesham/sonarpoc/HishamTest.kt
@@ -0,0 +1,17 @@
+package com.hesham.sonarpoc.ui.main
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class HishamTest {
+    @Test
+    fun addition_isCorrect() {
+        Hisham().add(2,3)
+    }
+}
