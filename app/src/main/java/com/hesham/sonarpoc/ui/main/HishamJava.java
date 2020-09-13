57861b7c7ca06b7fbb22c3d62d84f82d250883a8
Hesham Bakr <hesham.bakr@talabat.com>
2020-09-13 13:35:12 +0400

    progress

---
+++ app/src/main/java/com/hesham/sonarpoc/ui/main/HishamJava.java
@@ -0,0 +1,10 @@
+package com.hesham.sonarpoc.ui.main;
+
+public class HishamJava {
+    public int add(int a1, int a2){
+        return a1+a2;
+    }
+    public int add2(int a1, int a2){
+        return a1+a2;
+    }
+}
---
+++ app/src/test/java/com/hesham/sonarpoc/HishamJavaTest.java
@@ -0,0 +1,14 @@
+package com.hesham.sonarpoc;
+
+import com.hesham.sonarpoc.ui.main.HishamJava;
+
+import org.junit.Test;
+
+public class HishamJavaTest {
+
+    @Test
+    void addition_isCorrect() {
+        new HishamJava().add(2,3);
+    }
+
+}
